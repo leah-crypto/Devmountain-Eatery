78547bdc46ad4864d28d1b007e42f68c591099c8
Leah Roberson <77908412+leah-crypto@users.noreply.github.com>
2023-01-30 12:12:03 -0600

    update 6

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -11,11 +11,9 @@ const DetailScreen = () => {
   const { id } = useParams();
 
   const [recipe, setRecipe] = useState({});
-  const url = 'https://recipes.devmountain.com';
+  const url = "https://recipes.devmountain.com";
 
-console.log(id); //nothing in our obj
-
-
+  console.log(id); //nothing in our obj
 
   useEffect(() => {
     axios.get(`${url}/recipes/${id}`).then((res) => {
@@ -35,30 +33,36 @@ console.log(id); //nothing in our obj
         backgroundSize: "cover",
       }}
     >{recipe.recipe_name}</div> */}
-      <DetailImage image={recipe.image_url} title={recipe.recipe_name}/>
-     <div className={styles.contain}>
-      <div className={styles.ingredients_container}>
-        <h3><center>Recipe</center></h3>
-        <h4>Prep Time: {recipe.prep_time}</h4>
-        <h4>Cook Time: {recipe.cook_time}</h4>
-        <h4>Serves: {recipe.serves}</h4>
-        <br />
-        <h3><center>Ingredients</center></h3>
-        {recipe.ingredients &&
-          recipe.ingredients.map((ing, index) => {
-            return (
-              <h4>
-                {ing.quantity} {ing.ingredient}
-              </h4>
-            );
-          })}
-      </div>
-      <div className={styles.instruction_container}>
-        <h3><center>Instructions</center></h3>
-        <p style={{ whiteSpace: "pre-wrap" }}>
-          {recipe.instructions && JSON.parse(recipe.instructions)}
-        </p>
-      </div>
+      <DetailImage image={recipe.image_url} title={recipe.recipe_name} />
+      <div className={styles.contain}>
+        <div className={styles.ingredients_container}>
+          <h3>
+            <center>Recipe</center>
+          </h3>
+          <h4>Prep Time: {recipe.prep_time}</h4>
+          <h4>Cook Time: {recipe.cook_time}</h4>
+          <h4>Serves: {recipe.serves}</h4>
+          <br />
+          <h3>
+            <center>Ingredients</center>
+          </h3>
+          {recipe.ingredients &&
+            recipe.ingredients.map((ing, index) => {
+              return (
+                <h4>
+                  {ing.quantity} {ing.ingredient}
+                </h4>
+              );
+            })}
+        </div>
+        <div className={styles.instruction_container}>
+          <h3>
+            <center>Instructions</center>
+          </h3>
+          <p style={{ whiteSpace: "pre-wrap" }}>
+            {recipe.instructions && JSON.parse(recipe.instructions)}
+          </p>
+        </div>
       </div>
     </section>
   );
